993d003bf81cd2bc1a51b18ddc100f636674183d
AlexLevanchenko <alex.levanchenko@gmail.com>
2022-04-14 23:34:10 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        byte a = 127;
+        System.out.println(a);
+        a++;
+        System.out.println(a);
+    }
+}
